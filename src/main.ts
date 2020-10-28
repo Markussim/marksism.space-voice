ee90cbb52ab90b7d95325cb05ac9b8a589981ba5
Markussim <markus.simonsen@gmail.com>
2020-10-28 17:42:41 +0100

    Fixed html escape

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import express from 'express';
 import mongo from 'mongoose';
 const Stream = require('stream')
 import { MongoConnect } from './mongo';
+var eescape = require('escape-html');
 
-const readableStream = new Stream.Readable()
+//const readableStream = new Stream.Readable()
 
-readableStream._read = () => {}
+//readableStream._read = () => {}
 
 //const mongoose = require('mongoose');
 MongoConnect.cnctDB("MarksismVoice")
@@ -38,16 +39,16 @@ function createEntry(message: any) {
 
 app.use(express.static(clientdir))
 
-app.get('/', (req, res) => res.redirect(307, '/chatt'))
+app.get('/', (req, res) => res.redirect(307, '/get_chat'))
 
-app.post('/chatt', (req, res) => {
+app.post('/post_chat', (req, res) => {
     console.log("Post")
-    readableStream.push("New data")
+    //readableStream.push("New data")
     MongoConnect.saveToDB(createEntry(req.body.message))
-    res.redirect(307, '/chatt')
+    res.send('<meta http-equiv="Refresh" content="0; url=\'/get_chat\'" />')
 })
 
-app.get('/chatt', async (req, res) => {
+app.get('/get_chat', async (req, res) => {
 
     let messages = await MongoConnect.getCursor(messageDoc)
 
@@ -55,16 +56,16 @@ app.get('/chatt', async (req, res) => {
 
     //console.log(messages);
 
-    res.render("pages/index.ejs", { messages: messages })
+    res.render("pages/index.ejs", { messages: messages, eescape: eescape })
 })
 
-app.get('/update', (req, res) => {
+/*app.get('/update', (req, res) => {
     console.log("Got update request")
 
     readableStream.on('readable', () => {
         res.send("An update is availeble")
         console.log("Finished update request")
     });
-});
+});*/
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
